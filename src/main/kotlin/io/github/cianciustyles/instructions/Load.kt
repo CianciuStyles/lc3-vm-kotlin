54400a1839be3509177c2890778cd5e039aec455
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2020-10-03 15:24:11 +0100

    Implement Load instruction
    * Add loadAndSetConditionCodes in Instruction
    * Correct test names in LoadIndirectTest

--- src/main/kotlin/io/github/cianciustyles/instructions/Load.kt
+++ src/main/kotlin/io/github/cianciustyles/instructions/Load.kt
@@ -4,18 +4,15 @@ import io.github.cianciustyles.Memory
 import io.github.cianciustyles.Registers
 
 @ExperimentalUnsignedTypes
-class LoadIndirect(encoding: UShort) : Instruction() {
+class Load(encoding: UShort) : Instruction() {
     val destinationRegister: UShort = (encoding.toInt() shr 9 and 0x7).toUShort()
     val pcOffset: Short = extendSign(encoding.toInt() and 0x1FF, 9)
 
-    override fun execute(memory: Memory, registers: Registers) {
-        val newOffset: UShort = (registers.getPC() + pcOffset).toUShort()
-        val addressToLoad: UShort = memory[newOffset].toUShort()
+    override fun execute(memory: Memory, registers: Registers) =
         loadAndSetConditionCodes(
             memory,
             registers,
-            addressToLoad,
+            (registers.getPC() + pcOffset).toUShort(),
             destinationRegister
         )
-    }
 }
