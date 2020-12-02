2ab382452adfe2c7108fedb70a74173d9c5cb881
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2020-12-02 09:05:16 +0000

    Add KBSR handling in Memory

--- src/main/kotlin/io/github/cianciustyles/Memory.kt
+++ src/main/kotlin/io/github/cianciustyles/Memory.kt
@@ -1,5 +1,7 @@
 package io.github.cianciustyles
 
+import io.github.cianciustyles.Utils.readCharacter
+
 @ExperimentalUnsignedTypes
 class Memory(
     private val memory: ShortArray = ShortArray(UShort.MAX_VALUE.toInt())
@@ -9,8 +11,17 @@ class Memory(
         const val KBDR = 0xFE02
     }
 
-    operator fun get(index: UShort): Short =
-        memory[index.toInt()]
+    operator fun get(index: UShort): Short {
+        if (index == KBSR.toUShort()) {
+            val characterRead = readCharacter(false)
+            if (characterRead >= 0) {
+                memory[KBSR] = (1 shl 15).toShort()
+                memory[KBDR] = characterRead
+            }
+        }
+
+        return memory[index.toInt()]
+    }
 
     operator fun set(index: UShort, value: Short) {
         memory[index.toInt()] = value
